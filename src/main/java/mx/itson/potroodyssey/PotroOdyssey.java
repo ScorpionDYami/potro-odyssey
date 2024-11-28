dbd52c06e7d1467ddce01668038952ccb1f91ca0
Kevin Espinoza <62354142+ScorpionDYami@users.noreply.github.com>
2024-11-27 21:44:55 -0700

    Creación del proyecto

---
+++ src/main/java/mx/itson/potroodyssey/PotroOdyssey.java
@@ -0,0 +1,13 @@
+
+package mx.itson.potroodyssey;
+
+/**
+ *
+ * @author Equipo C
+ */
+public class PotroOdyssey {
+
+    public static void main(String[] args) {
+        System.out.println("I'm a Potromon");
+    }
+}
